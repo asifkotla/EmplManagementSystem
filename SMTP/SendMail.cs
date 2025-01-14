093bf042dcc5dbd2b88e286ab8565ad2ce509dd8
ASIF KOTLA <157131935+asifkotla@users.noreply.github.com>
2025-01-14 20:23:50 +0530

    Update SendMail.cs

--- SMTP/SendMail.cs
+++ SMTP/SendMail.cs
@@ -18,8 +18,8 @@ namespace EmplManagementSystem.SMTP
         public string Body { get; set; }
 
 
-        string fromemail = "employeeemanagementsystem@gmail.com";
-        string frompass = "hnnq loam hhja romh";
+        string fromemail = "Your Email";
+        string frompass = "Your APP Password";
 
         public void SendEmail(SendMail sendMail)
         {
